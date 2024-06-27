14c00b5f2e3b3b18c37b169ab06c8432ee3af0c1
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-06-27 18:10:19 +0200

    [UPD] Created else if{} for multiples of 3

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 // Creo un ciclo for nel quale costruisco un iterazione per una variabile contatore
 
 for (let i = 1; i <= 100; i++){
-    // Se è sia multiplo di 3 che di 5 allora stampa "FIzzBuzz"
+    // Se è sia multiplo di 3 che di 5 allora stampa "FizzBuzz"
     if(i % 3 == 0 && i % 5 == 0){
         console.log( `FizzBuzz`)
     }
+    // Se è  multiplo di 3  allora stampa "Fizz"
+    else if(i % 3 == 0 ){
+        console.log( `Fizz`)
+    }
+    
 }
+
