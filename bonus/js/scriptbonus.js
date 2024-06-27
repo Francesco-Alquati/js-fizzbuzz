45baca8cdf67f52e64cd15a60b525ab7c77e1bd1
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-06-27 18:35:07 +0200

    [UPD] Bonus 1

--- bonus/js/scriptbonus.js
+++ bonus/js/scriptbonus.js
@@ -1,22 +1,28 @@
-// Creo un ciclo for nel quale costruisco un iterazione per una variabile contatore
+// Recupero la lista dal DOM
+const list = document.getElementById('list');
 
+// Creo un ciclo for nel quale costruisco un iterazione per una variabile contatore
 for (let i = 1; i <= 100; i++){
+
+    let list_item = document.createElement('li');
+    list_item.innerText = `${i}`;
+    
     // Se è sia multiplo di 3 che di 5 allora stampa "FizzBuzz" al posto del numero 
     if(i % 3 == 0 && i % 5 == 0){
-        console.log( `FizzBuzz`);
+        list_item.innerText = `FizzBuzz`;
     }
     // Se è  multiplo di 3  allora stampa "Fizz" al posto del numero 
     else if(i % 3 == 0 ){
-        console.log( `Fizz`);
+        list_item.innerText = `Fizz`;
     }
     // Se è  multiplo di 5  allora stampa "Buzz" al posto del numero 
     else if(i % 5 == 0){
-        console.log( `Buzz`);
+        list_item.innerText = `Buzz`;
     }
     // Sennò stampa il numero 
     else{
-        console.log(`${i}`);
+        list_item.innerText = `${i}`;
     }
-    let list_item = document.createElement('li');
-    list_item.innerText = `${i}`;
+    
+    list.append(list_item);
 }
