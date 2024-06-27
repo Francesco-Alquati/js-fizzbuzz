50539b0fdeda4ec38b9fd17fee125a5fe9f7a332
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-06-27 20:11:30 +0200

    [UPD] Bonus 2

--- bonus/js/scriptbonus.js
+++ bonus/js/scriptbonus.js
@@ -1,28 +1,36 @@
 // Recupero la lista dal DOM
-const list = document.getElementById('list');
+// const list = document.getElementById('list');
+const board = document.querySelector('.board');
 
 // Creo un ciclo for nel quale costruisco un iterazione per una variabile contatore
 for (let i = 1; i <= 100; i++){
 
-    let list_item = document.createElement('li');
-    list_item.innerText = `${i}`;
+    // let list_item = document.createElement('li');
+    // list_item.innerText = `${i}`;
+
+    const square = document.createElement('div');
+    square.classList.add('square');
     
     // Se è sia multiplo di 3 che di 5 allora stampa "FizzBuzz" al posto del numero 
     if(i % 3 == 0 && i % 5 == 0){
-        list_item.innerText = `FizzBuzz`;
+        square.innerText = `FizzBuzz`;
+        square.classList.add('bg-red');
     }
     // Se è  multiplo di 3  allora stampa "Fizz" al posto del numero 
     else if(i % 3 == 0 ){
-        list_item.innerText = `Fizz`;
+        square.innerText = `Fizz`;
+        square.classList.add('bg-green');
     }
     // Se è  multiplo di 5  allora stampa "Buzz" al posto del numero 
     else if(i % 5 == 0){
-        list_item.innerText = `Buzz`;
+        square.innerText = `Buzz`;
+        square.classList.add('bg-yellow');
     }
     // Sennò stampa il numero 
     else{
-        list_item.innerText = `${i}`;
+        square.innerText = `${i}`;
+        square.classList.add('bg-blue');
     }
-    
-    list.append(list_item);
+    board.append(square);
+    // list.append(list_item);
 }
