d66ee2707a4c3969ca2ac05b667028f20b9080f9
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-06-27 18:27:14 +0200

    [UPD] Created bonus folder

---
+++ bonus/js/scriptbonus.js
@@ -0,0 +1,22 @@
+// Creo un ciclo for nel quale costruisco un iterazione per una variabile contatore
+
+for (let i = 1; i <= 100; i++){
+    // Se è sia multiplo di 3 che di 5 allora stampa "FizzBuzz" al posto del numero 
+    if(i % 3 == 0 && i % 5 == 0){
+        console.log( `FizzBuzz`);
+    }
+    // Se è  multiplo di 3  allora stampa "Fizz" al posto del numero 
+    else if(i % 3 == 0 ){
+        console.log( `Fizz`);
+    }
+    // Se è  multiplo di 5  allora stampa "Buzz" al posto del numero 
+    else if(i % 5 == 0){
+        console.log( `Buzz`);
+    }
+    // Sennò stampa il numero 
+    else{
+        console.log(`${i}`);
+    }
+    let list_item = document.createElement('li');
+    list_item.innerText = `${i}`;
+}
