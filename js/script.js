68e5d1ba984c25f671ac439ba0262e2c0757591e
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-06-27 18:23:46 +0200

    [UPD] Small fixes

--- js/script.js
+++ js/script.js
@@ -3,19 +3,18 @@
 for (let i = 1; i <= 100; i++){
     // Se è sia multiplo di 3 che di 5 allora stampa "FizzBuzz" al posto del numero 
     if(i % 3 == 0 && i % 5 == 0){
-        console.log( `FizzBuzz`)
+        console.log( `FizzBuzz`);
     }
     // Se è  multiplo di 3  allora stampa "Fizz" al posto del numero 
     else if(i % 3 == 0 ){
-        console.log( `Fizz`)
+        console.log( `Fizz`);
     }
     // Se è  multiplo di 5  allora stampa "Buzz" al posto del numero 
     else if(i % 5 == 0){
-        console.log( `Buzz`)
+        console.log( `Buzz`);
     }
     // Sennò stampa il numero 
     else{
-        console.log(`${i}`)
+        console.log(`${i}`);
     }
-    
 }
